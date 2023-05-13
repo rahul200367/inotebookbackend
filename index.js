558f036ef62f3ee5fe30be2b89b6de9d69de21e5
rahul <raulkaus07@gmail.com>
2023-05-13 22:33:05 +0530

    4

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors  = require('cors');
 connectToMongo();
 const app = express()
 const port = 5000
-app.use(cors());
+app.use(cors({
+  origin:true
+}));
 app.use(express.json())
 //available routes
 app.use('/api/auth',require('./routes/auth'))
