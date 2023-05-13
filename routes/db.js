e60226a44234daa79c009910326f0ae946e46821
rahul <raulkaus07@gmail.com>
2023-05-13 23:04:15 +0530

    commit

--- routes/db.js
+++ routes/db.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoURl = 'mongodb+srv://rahulkaus07:Lb1yKcQloK8vVZkD@cluster0.quefu51.mongodb.net/inotebook?retryWrites=true&w=majority';
 //const mongoURl = 'mongodb://localhost:27017/ecom?directConnection=true';
+mongoose.set('strictQuery', false);
 const  connectToMongo = ()=>{
     mongoose.connect(mongoURl, ()=>{
         console.log('connected to mongo sussesfully');
